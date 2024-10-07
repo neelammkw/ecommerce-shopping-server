eba63f1441f2b136308e14009da7d1d530692115
Neelam Makwana <hiyasoni5@gmail.com>
2024-10-07 10:38:43 +0530

    first commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const {User} = require('../models/User'); // Adjust the path as needed
+const {User} = require('../models/user'); // Adjust the path as needed
 
 const authMiddleware = async (req, res, next) => {
   // Check if the Authorization header exists
